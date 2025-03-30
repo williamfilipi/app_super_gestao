d764490818d5e2515b22d57d3c9c04f63d1d7456
William Filipi <williamfilipi195@gmail.com>
2025-03-30 00:02:40 -0300

    AprendendoTemplates

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,16 @@ Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', 'LoginController@login')->name('site.login');
 
 //Agrupa as rotas formando uma rota onde apenas pessoas em especificas terão acesso 
-Route::prefix('/app')->group(function(){
+Route::prefix('/app')->group(function () {
     Route::get('/clientes', 'ClientesController@clientes')->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedor');
-    Route::get('/produtos', 'ProdutosController@produtos')->name('app.produtos');    
+    Route::get('/produtos', 'ProdutosController@produtos')->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
-Route::fallback(function(){
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para a página inicial';
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">clique aqui</a> para ir para a página inicial';
 });
 
 //{mensagem?} -> ? parameter optional os parâmetros podem opcionais desde que sejam passados da direita para esquerda
